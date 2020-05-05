5156489ff7bd79a65d4e68ed9ab7ae8ca2036c57
Zhao Yi Yap <s10196983@connect.np.edu.sg>
2020-05-05 18:46:47 +0800

    Finished practical 3

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -3,12 +3,29 @@ package sg.edu.np.mad.week3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declare Variables
+    MediaPlayer babyLaughMedia;
+    VideoView rabbidVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        babyLaughMedia = MediaPlayer.create(this, R.raw.baby_laughing);
+        babyLaughMedia.start();
+
+        rabbidVideo = (VideoView) findViewById(R.id.videoView);
+        rabbidVideo.setVideoURI(
+                Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid)
+        );
+        rabbidVideo.start();
     }
+
 }
